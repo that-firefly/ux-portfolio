9e038013ff190a09ce39a18ac6236cfbd0f24cbe
that-firefly <54668579+that-firefly@users.noreply.github.com>
2023-11-30 13:28:14 +0000

    first testing virtual commit

--- DEFRA-PROTO-25SEPT/cpr-prototype-that-firefly-defra/cpr-prototype-that-firefly-defra/app/assets/javascripts/loading-file-errors.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    // Handler for .ready() called.
-    window.setTimeout(function () {
-        location.href = "./errors.html";
-    }, 3000);
-});
--- DEFRA-PROTO-25SEPT/cpr-prototype-that-firefly-defra/cpr-prototype-that-firefly-defra/app/assets/javascripts/loading-file-success.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    // Handler for .ready() called.
-    window.setTimeout(function () {
-        location.href = "./check-file.html";
-    }, 5000);
-});
