ecdde2820409f8992a9f8a2cd10e4cacf07a6bbc
Estela Gaspar <54668579+that-firefly@users.noreply.github.com>
2025-03-03 16:25:22 +0000

    Update estela-ux.js
    Updating Java

--- estela-ux.js
+++ estela-ux.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     const menu = document.querySelector('.menu');
     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector("nav ul");
+
+    hamburger.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
